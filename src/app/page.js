4d5ffcd24029d65b001cc81bd8a7d227ef7cbf8e
MS Kang <tomskang@naver.com>
2025-05-10 23:53:04 +0900

    메인 페이지에 global.css 적용

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,15 @@
 
 import NavBar from '@/app/components/NavBar';
 import '@/styles/index.css';
+// Styles
+import '@/styles/global.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div id="whole-container">
       <NavBar />
       <main className="main">
-        <a href="/make" className="button">
+        <a href="/make" className="wide-button">
           새로운 추억 만들기
         </a>
       </main>
