c2d4b7508f654db580f3e5fb47f2e5294055b371
leehyungju <dlgudwn1219@gmail.com>
2025-05-11 00:08:17 +0900

    Second photo2video

--- src/app/photo2video/page.js
+++ src/app/photo2video/page.js
@@ -7,6 +7,7 @@ export const dynamic = "force-dynamic";
 
 async function fetchPhotos() {
   // 예시 – 실제로는 DB·REST 등에서 받아오세요
+  
   const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + "/api/get-total");
   return res.json(); // [{ id, s3Url, createdAt, ...}, ...]
 }
