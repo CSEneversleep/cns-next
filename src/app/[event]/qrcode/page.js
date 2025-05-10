62c1a62d3bd91244d05e6556f79aa64ed983dea3
MS Kang <tomskang@naver.com>
2025-05-11 05:28:01 +0900

    feat: fabicon 제작, qrcode 수정

--- src/app/[event]/qrcode/page.js
+++ src/app/[event]/qrcode/page.js
@@ -1,16 +1,22 @@
 import { getEvent } from "@/utils/firebase/event";
 import QRCode from "react-qr-code";
+import { headers } from "next/headers";
 
 export default async function QRPage({ params }) {
-  const URL = process.env.NEXT_PUBLIC_URL;
-  const event = params.event;
+  const event = await params.event;
   const eventData = await getEvent(event);
   const title = eventData?.eventname || "이벤트 없음";
+
+  // 요청 헤더에서 호스트 정보 가져오기
+  const headersList = await headers();
+  const host = headersList.get("host");
+  const protocol = host?.includes("localhost") ? "http" : "https";
+  const fullURL = `${protocol}://${host}/${event}`;
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>{title} QR Code</h1>
-      <QRCode value={`${URL}/${event}`} size={300} />
+      <QRCode value={fullURL} size={300} />
     </div>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,19 @@
 import '@/styles/global.css';
 import StarField from '@/components/StarField';
 
+// Default metadata
+export const metadata = {
+  title: "Afterglow",
+  description: "제 3회 SKYST CSE: Never Sleeps의 최종 결과물",
+  openGraph: {
+    title: "Afterglow",
+    description: "제 3회 SKYST CSE: Never Sleeps의 최종 결과물",
+    siteName: "Afterglow",
+    type: "website",
+  },
+  icons: { icon: "/opengraph/favicon.png", },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
