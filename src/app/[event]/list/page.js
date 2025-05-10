84f60779d72cf4b6a42b951134302a280933e4cf
insuhkim <insuhkim@naver.com>
2025-05-11 04:52:49 +0900

    fix: Fix showing event name

--- src/app/[event]/list/page.js
+++ src/app/[event]/list/page.js
@@ -1,19 +1,22 @@
 // app/[event]/list/page.js
 
-import PhotoGallery from '@/components/PhotoGallery';
-import { getTotalImage } from '@/utils/getImage';  // Firebase 유틸 함수
+import PhotoGallery from "@/components/PhotoGallery";
+import { getTotalImage } from "@/utils/getImage"; // Firebase 유틸 함수
+import { getEvent } from "@/utils/firebase/event";
 
 export default async function ListPage({ params }) {
-    const { event } = await params;
+  const { event } = await params;
+  const eventData = await getEvent(event);
+  const title = eventData?.eventname || "이벤트 없음";
 
-    const rawPhotos = await getTotalImage(event);
-    const photos = JSON.parse(JSON.stringify(rawPhotos));
+  const rawPhotos = await getTotalImage(event);
+  const photos = JSON.parse(JSON.stringify(rawPhotos));
 
-    return (
-        <div className="p-6">
-        <h1 className="text-2xl mb-4 ml-4">{event} 행사의 사진 목록</h1>
-        {/* photos를 그대로 Client 컴포넌트로 넘김 */}
-        <PhotoGallery photos={photos} />
-        </div>
-    );
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl mb-4 ml-4">{title} 행사의 사진 목록</h1>
+      {/* photos를 그대로 Client 컴포넌트로 넘김 */}
+      <PhotoGallery photos={photos} />
+    </div>
+  );
 }
--- src/app/[event]/qrcode/page.js
+++ src/app/[event]/qrcode/page.js
@@ -1,14 +1,15 @@
-"use client";
-import { useParams } from "next/navigation";
+import { getEvent } from "@/utils/firebase/event";
 import QRCode from "react-qr-code";
 
-export default function QRPage() {
+export default async function QRPage({ params }) {
   const URL = process.env.NEXT_PUBLIC_URL;
-  const { event } = useParams();
+  const event = params.event;
+  const eventData = await getEvent(event);
+  const title = eventData?.eventname || "이벤트 없음";
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>{event} QR Code</h1>
+      <h1>{title} QR Code</h1>
       <QRCode value={`${URL}/${event}`} size={300} />
     </div>
   );
