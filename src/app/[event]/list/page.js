2bacd5c61fddefd53ffbe869d1e86f51702d8564
2024-10361 <birmjunekim@snu.ac.kr>
2025-05-11 00:23:14 +0900

    사진 list 크기 맞춤

--- src/app/[event]/list/page.js
+++ src/app/[event]/list/page.js
@@ -11,7 +11,7 @@ export default async function ListPage({ params }) {
 
     return (
         <div className="p-6">
-        <h1 className="text-2xl mb-4">{event} 행사의 사진 목록</h1>
+        <h1 className="text-2xl mb-4 ml-4">{event} 행사의 사진 목록</h1>
         {/* photos를 그대로 Client 컴포넌트로 넘김 */}
         <PhotoGallery photos={photos} />
         </div>
